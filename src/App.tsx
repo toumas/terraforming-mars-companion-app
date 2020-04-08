7158508987dbc5c277a987c8652b59a608335657
Tuomas Ukkola <tuomasukk@gmail.com>
2020-04-08 19:47:29 +0300

    Add basic sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from './store';
-import Section from './Section';
+import {Section} from './Section';
+import {SectionNames} from './section';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Section />
+      <Section name={SectionNames.STEEL} />
+      <Section name={SectionNames.TITANIUM} />
+      <Section name={SectionNames.PLANTS} />
+      <Section name={SectionNames.ENERGY} />
+      <Section name={SectionNames.HEAT} />
     </Provider>
   );
 };
