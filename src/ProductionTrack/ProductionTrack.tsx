88d52af1cf9cdf8f523f13445523d55b2e9252d4
Tuomas Ukkola <tuomasukk@gmail.com>
2020-04-08 22:57:54 +0300

    Make ProductionTrack layout a row

--- src/ProductionTrack/ProductionTrack.tsx
+++ src/ProductionTrack/ProductionTrack.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, Button} from 'react-native';
+import {Text, Button, View, StyleSheet} from 'react-native';
 
 interface ProductionTrackProps {
   handleOnDecrement: () => void;
@@ -7,16 +7,25 @@ interface ProductionTrackProps {
   value: number;
 };
 
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+})
+
 export function ProductionTrack({
   value,
   handleOnDecrement,
   handleOnIncrement,
 }: ProductionTrackProps) {
   return (
-    <>
+    <View style={styles.container}>
       <Button title='-' onPress={handleOnDecrement} />
       <Text>{value}</Text>
       <Button title='+' onPress={handleOnIncrement} />
-    </>
+    </View>
   );
 }
