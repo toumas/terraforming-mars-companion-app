ad8771ade98abe562e58a4d4863c31019fbba46a
Tuomas Ukkola <tuomasukk@gmail.com>
2020-04-10 13:54:55 +0300

    Add megaCredits Section

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { SectionNames } from './SectionNames';
 const App = () => {
   return (
     <Provider store={store}>
+      <Section name={SectionNames.MEGA_CREDITS} />
       <Section name={SectionNames.STEEL} />
       <Section name={SectionNames.TITANIUM} />
       <Section name={SectionNames.PLANTS} />
