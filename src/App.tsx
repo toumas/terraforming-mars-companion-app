8e34b4b9a2fddcb2252850fb8e8ff18f95f7b2bb
Tuomas Ukkola <tuomasukk@gmail.com>
2020-05-12 22:52:27 +0300

    Refactor App to use PlayerBoard

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {store} from './store';
-import {Section} from './Section';
-import {SectionNames} from './SectionNames';
+import {PlayerBoard} from './PlayerBoard';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Section name={SectionNames.MEGA_CREDITS} />
-      <Section name={SectionNames.STEEL} />
-      <Section name={SectionNames.TITANIUM} />
-      <Section name={SectionNames.PLANTS} />
-      <Section name={SectionNames.ENERGY} />
-      <Section name={SectionNames.HEAT} />
+      <PlayerBoard />
     </Provider>
   );
 };
