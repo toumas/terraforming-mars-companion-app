d610b04c7c5e07d76ee7ee8d55c94bd405c552a5
Tuomas Ukkola <tuomasukk@gmail.com>
2020-06-17 19:32:45 +0300

    Update UI to support new features
    Firstly react-native-action-button replaced react-native-floating-
    action. Then packages react-native-svg and react-native-svg-transformer
    were needed to support svg icons. Finally used ActionButton component to
    allow updating of generation and terraform rating. Also display current
    values of those in text.

--- src/PlayerBoard.tsx
+++ src/PlayerBoard.tsx
@@ -1,39 +1,66 @@
 import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
-import {FloatingAction} from 'react-native-floating-action';
+import ActionButton from 'react-native-action-button';
 import {Section} from './Section';
 import {SectionNames} from './SectionNames';
-import {incrementGeneration} from './generation';
+import {incrementGeneration, selectGeneration} from './generation';
 import {selectEnergyResources} from './section';
+import {
+  incrementTerraformRating,
+  selectTerraformRating,
+} from './terraformRating';
 
-const actions = [
-  {
-    icon: require('./cross.png'),
-    name: 'add',
-  },
-];
+import Globe from './globe.svg';
+import UserPlus from './user-plus.svg';
 
 export const PlayerBoard = () => {
   const dispatch = useDispatch();
   const energyResources = useSelector(selectEnergyResources);
+  const generation = useSelector(selectGeneration);
+  const terraformRating = useSelector(selectTerraformRating);
 
-  function handleFABPress(): void {
+  function handleIncrementGeneration(): void {
     dispatch(incrementGeneration(energyResources));
+  }
+
+  function handleIncrementTerraformRating(): void {
+    dispatch(incrementTerraformRating());
   }
 
   return (
     <>
+      <View style={styles.globalsContainer}>
+        <Text>Generation: {generation}</Text>
+        <Text>Terraform rating: {terraformRating}</Text>
+      </View>
       <Section name={SectionNames.MEGA_CREDITS} />
       <Section name={SectionNames.STEEL} />
       <Section name={SectionNames.TITANIUM} />
       <Section name={SectionNames.PLANTS} />
       <Section name={SectionNames.ENERGY} />
       <Section name={SectionNames.HEAT} />
-      <FloatingAction
-        overrideWithAction={true}
-        actions={actions}
-        onPressItem={handleFABPress}
+      <ActionButton
+        buttonColor="#ff5252"
+        renderIcon={() => <UserPlus />}
+        degrees={0}
+        onPress={handleIncrementGeneration}
+        offsetY={100}
+      />
+      <ActionButton
+        buttonColor="#ff5252"
+        renderIcon={() => <Globe />}
+        degrees={0}
+        onPress={handleIncrementTerraformRating}
       />
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  globalsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
